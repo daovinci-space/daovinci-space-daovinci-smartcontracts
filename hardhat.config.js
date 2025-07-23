32cdda57238e4ffc7892f916cea1aaf545471be6
Murillo-DaoVinci <contact@daovinci.space>
2025-07-23 20:16:49 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,14 @@
 // hardhat.config.js - Estrutura inicial dos smart contracts DAO VINCI
+]require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+
+    base_goerli: {
+      url: "https://goerli.base.org", // RPC da BASE testnet
+      accounts: [process.env.PRIVATE_KEY], // chave privada via variável de ambiente
+    },
+  },
+};
